a1de66454dedc9b341adb26e676d9595a3288491
djsiclait <djidjellypas92@gmail.com>
2016-07-07 18:50:07 -0400

    Minor hikcup

---
+++ src/main/java/Client/PreMatrixJavaClient.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Siclait on 7/7/16.
+ */
+
+package Client;
+
+import java.net.URL;
+
+public class PreMatrixJavaClient {
+    public static void main(String[] args) throws Exception {
+
+        URL url = new URL("http:localhost:9999/ws/PreMatrix?wsdl");
+    }
+}
